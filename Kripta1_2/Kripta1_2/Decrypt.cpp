d499a9cabe66fdca894db85d196eebcaf6d9215c
Flex2424 <fakestratocaster@yandex.ru>
2016-11-07 18:52:00 +0400

    Try to reconstruct the key

--- Kripta1_2/Kripta1_2/Decrypt.cpp
+++ Kripta1_2/Kripta1_2/Decrypt.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <vector>
 #include <string.h>
 #include <malloc.h>
 
@@ -96,21 +97,105 @@ void crypto_analys() {
 	ifstream plain_text;
 	string filename = "";
 	cout << "\tCryptoanalys for transposition cipher" << endl;
-	cout << "Enter the file name: ";
-	getline(cin, filename);
+	//cout << "Enter the file name (plain text): ";
+	//getline(cin, filename);
+	filename = "data";
 	plain_text.open(filename, ios::in, ios::binary);
-	string plain_text_part = "";
+	string plain_text_part = ""; //4 symbols from plain text
+	string block = ""; //16 sybmols from cipher text
 	for (int i = 0; i < 4; i++) {
 		char ch = plain_text.get();
 		plain_text_part.push_back(ch);
 	}
 	plain_text.close();
+	ifstream cipher_text;
+	filename = "";
+	//cout << "Enter the file name (cipher text): ";
+	//getline(cin, filename);
+	filename = "transposition";
+	cipher_text.open(filename, ios::in, ios::binary);
+	for (int i = 0; i < 16; i++) {
+		char ch = cipher_text.get();
+		block.push_back(ch);
+	}
+	cipher_text.close();
 	for (int i = 0; i < plain_text_part.size(); i++)
 		cout << plain_text_part[i];
 	cout << endl << endl;
+	for (int i = 0; i < block.size(); i++)
+		cout << block[i];
+	cout << endl << endl;
 
+	vector<int> reconstructed_key(16, 17);
 
+	for (int i = 0; i < plain_text_part.size(); i++) {
+		char ch = plain_text_part[i];
+		int j = 0;
+		vector<int> choises;
+		for (j = 0; j < block.size(); j++) {
+			if (block[j] == ch)
+				choises.push_back(j);
+		}
+		short position;
+		//if positions with same symbol > 1
+		if (choises.size() != 1) {
+			cout << "Symbol '" << ch << "' found on " << choises.size() << "positions." << endl;
+			cout << "Positions: ";
+			for (int k = 0; k < choises.size(); k++)
+				cout << choises[k] << " ";
+			cout << endl << "Enter needed position: ";
+			bool flag = false;
+			//check entered position
+			while (flag == false) {
+				cin >> position;
+				short res = count(choises.begin(), choises.end(), position);
+				if (res == 0)
+					cout << "Wrong position. Try again" << endl;
+				else 
+					flag = true;
 
+			}
+		}
 
+		if (choises.size() == 1)
+			position = choises[0];
+		reconstructed_key[position] = i;
+	}
+
+	for (int i = 0; i < reconstructed_key.size(); i++)
+		cout << reconstructed_key[i] << " ";
+	cout << endl;
+
+	vector<int> copy_reconstructed_key = reconstructed_key;
+	int next = 4;
+	int tmp;
+	for (int i = 0; i < 10; i++) {
+		tmp = next;
+		next++;
+		for (int j = 0; j < copy_reconstructed_key.size(); j++) {
+			if (tmp > 15) {
+				tmp = tmp - 12;
+			}
+			if (copy_reconstructed_key[j] == 17) {
+				copy_reconstructed_key[j] = tmp;
+				tmp++;
+				
+			}				
+			else {
+				continue;
+			}
+				
+		}
+
+		for (int j = 0; j < copy_reconstructed_key.size(); j++)
+			cout << copy_reconstructed_key[j] << " ";
+		cout << endl;
+		/*
+		Here: decrypt.
+		Ask good key or continue
+		*/
+		copy_reconstructed_key = reconstructed_key;
+
+	}
 
 }
