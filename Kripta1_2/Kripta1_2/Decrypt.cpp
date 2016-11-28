8d141487e1817dc7794f20df11d40a42c5661bb8
Flex2424 <fakestratocaster@yandex.ru>
2016-11-28 05:26:45 +0400

    Need to decipher transposition

--- Kripta1_2/Kripta1_2/Decrypt.cpp
+++ Kripta1_2/Kripta1_2/Decrypt.cpp
@@ -3,87 +3,34 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-#include <string.h>
 #include <malloc.h>
+#include <streambuf>
+#include <conio.h>
+#include <map>
 
 using namespace std;
 
-int l[500];
-int nods[500];
-int nl = 0;
-
-void kasiski();
 int nod(int a, int b);
 void crypto_analys();
+void friedmans_magic(int keylen, string alphabet);
+int kasisky();
+
+int l[3000];
+int nods[3000];
+int nl = 0;
 
 int main() {
-	//kasiski();
+	ifstream input_alph;
+	input_alph.open("alphabet", ios::binary);
+	string alphabet((istreambuf_iterator<char>(input_alph)), istreambuf_iterator<char>());
+	input_alph.close();
+	/*int keylen = kasisky();
+	friedmans_magic(keylen, alphabet);*/
 	crypto_analys();
-
 	return 0;
 }
 
-void kasiski() {
-	char *s = (char *)calloc(1, sizeof(char));
-	char fname[100];
-	char c;
-	unsigned int n = 0, i, j, keylen;
-	FILE *f;
-	printf("\tTest Kasiski: \n");
-	printf("file name: ");
-	scanf("%s", fname);
-	f = fopen(fname, "r+b");
-	if (f == NULL){
-		printf("file not found\n");
-		return;
-	}
 
-	for (i = 0; i < 500; ++i)
-		nods[i] = 0;
-
-
-	while (!feof(f))
-	if (fread(&c, 1, 1, f)){
-		s[n++] = c;
-		s = (char *)realloc(s, n + 1);
-	}
-
-	s[n] = '\0';
-
-	char str1[4], str2[4];
-
-	for (i = 0; i < strlen(s); ++i)
-	{
-		str1[0] = s[i];
-		str1[1] = s[i + 1];
-		str1[2] = s[i + 2];
-		str1[3] = '\0';
-
-		for (j = i + 1; j < strlen(s); ++j)
-		{
-			str2[0] = s[j];
-			str2[1] = s[j + 1];
-			str2[2] = s[j + 2];
-			str2[3] = '\0';
-
-			if (!strcmp(str1, str2)) l[nl++] = j - i;
-
-		}
-	}
-
-	for (i = 0; i < nl; ++i)
-	for (j = i + 1; j < nl; ++j)
-		nods[nod(l[i], l[j])]++;
-
-
-	keylen = 0;
-	for (i = 2; i < 500; ++i)
-		if (nods[keylen] < nods[i]) keylen = i;
-
-	printf("key's length is :%d\n", keylen);
-
-	free(s);
-}
 
 int nod(int a, int b)
 {
@@ -95,11 +42,7 @@ int nod(int a, int b)
 
 void crypto_analys() {
 	ifstream plain_text;
-	string filename = "";
-	cout << "\tCryptoanalys for transposition cipher" << endl;
-	//cout << "Enter the file name (plain text): ";
-	//getline(cin, filename);
-	filename = "data";
+	string filename = "plain_text";
 	plain_text.open(filename, ios::in, ios::binary);
 	string plain_text_part = ""; //4 symbols from plain text
 	string block = ""; //16 sybmols from cipher text
@@ -198,4 +141,177 @@ void crypto_analys() {
 
 	}
 
+}
+
+
+void friedmans_magic(int keylen, string alphabet) {
+	string *groups = new string[keylen];
+
+	ifstream input_text("vigenere", ios::binary);
+	string cipher_text((istreambuf_iterator<char>(input_text)), istreambuf_iterator<char>());
+	input_text.close();
+
+	ifstream fin_f("alph_and_freq", ios::binary);
+	map<char, double> frequencies;
+	string buf(sizeof(char)+sizeof(double), 0);
+	while (fin_f.read(&buf[0], buf.length()))
+	{
+		char* c = (char*)&buf[0];
+		double* f = (double*)&buf[sizeof(char)];
+		frequencies[*c] = *f;
+	}
+	fin_f.close();
+
+
+	int* c = new int[alphabet.size()];	
+	float delta = 0;
+	for (auto f : frequencies)
+		delta += f.second * f.second;
+	
+	for (int i = 0; i < cipher_text.length(); i++)
+		groups[i % keylen].push_back(cipher_text[i]);
+	for (int i = 0; i < alphabet.size(); i++)
+		c[i] = 0;
+	for (int i = 0; i < groups[0].length(); i++)
+		c[alphabet.find(groups[0][i])]++;
+
+
+	int* cc = new int[alphabet.size()];
+	int *shifts = new int[keylen];
+	float Ibest = 0.0;
+	for (int i = 1; i < keylen; i++)
+	{
+		shifts[i] = 0;
+		Ibest = 0;
+		for (int j = 0; j < alphabet.size(); j++)
+			cc[j] = 0;
+		for (int j = 0; j < groups[j].length(); j++)
+			cc[alphabet.find(groups[i][j])]++;
+		for (int j = 0; j < alphabet.size(); j++)
+			Ibest += c[j] * cc[j];
+		Ibest /= groups[0].length() * groups[i].length();
+
+		for (int j = 1; j < alphabet.length(); j++)
+		{
+			for (int k = 0; k < groups[i].length(); k++)
+				groups[i][k] = alphabet[(alphabet.find(groups[i][k]) + 1) % alphabet.length()];
+			for (int j = 0; j < alphabet.size(); j++)
+				cc[j] = 0;
+			for (int j = 0; j < groups[i].length(); j++)
+				cc[alphabet.find(groups[i][j])]++;
+			float I = 0;
+			for (int j = 0; j < alphabet.size(); j++)
+				I += c[j] * cc[j];
+			I /= groups[0].length() * groups[i].length();
+			if (abs(I - delta) < abs(Ibest - delta))
+			{
+				Ibest = I;
+				shifts[i] = j;
+			}
+		}
+	}
+	vector<int> tmp_key(keylen), key(keylen);
+	float beststat = 100500;
+	string plaintext2;
+	for (int i = 0; i < alphabet.length(); i++)
+	{
+		plaintext2.clear();
+		tmp_key[0] = i;
+		for (int i = 1; i < keylen; i++)
+			tmp_key[i] = (alphabet.length() + tmp_key[0] - shifts[i]) % alphabet.length();
+		for (int j = 0; j < cipher_text.length(); j++)
+		{
+			int index = alphabet.find(cipher_text[j]);
+			plaintext2.push_back(alphabet[(alphabet.size() + index - tmp_key[j % key.size()]) % alphabet.size()]);
+		}
+		map<char, double> frequencies2;
+		for (auto c : plaintext2)
+		{
+			if (frequencies2.count(c) == 0)
+				frequencies2[c] = 1.0;
+			else
+				frequencies2[c] += 1.0;
+		}
+		float stat = 0.0;
+		for (auto f : frequencies2)
+			stat += pow(f.second / plaintext2.length() - frequencies[f.first], 2.0);
+		if (stat < beststat)
+		{
+			beststat = stat;
+			key = tmp_key;
+		}
+	}
+	plaintext2.clear();
+	for (int j = 0; j < cipher_text.length(); j++)
+	{
+		int index = alphabet.find(cipher_text[j]);
+		plaintext2.push_back(alphabet[(alphabet.size() + index - key[j % key.size()]) % alphabet.size()]);
+	}
+	cout << plaintext2 << endl;
+
+	
+}
+
+int kasisky() {
+	char *s = (char *)calloc(1, sizeof(char));
+	char fname[100];
+	char c;
+	unsigned int n = 0, i, j, keylen;
+	FILE *f;
+	printf("file name: ");
+	scanf("%s", fname);
+	f = fopen(fname, "r");
+	if (f == NULL){
+		printf("file not found\n");
+		return 1;
+	}
+
+	for (i = 0; i<500; ++i)
+		nods[i] = 0;
+
+
+	while (!feof(f))
+	if (fread(&c, 1, 1, f)){
+		s[n++] = c;
+		s = (char *)realloc(s, n + 1);
+	}
+
+	s[n] = '\0';
+
+	char str1[4], str2[4];
+
+	for (i = 0; i<strlen(s); ++i)
+	{
+		str1[0] = s[i];
+		str1[1] = s[i + 1];
+		str1[2] = s[i + 2];
+		str1[3] = '\0';
+
+		for (j = i + 1; j<strlen(s); ++j)
+		{
+			str2[0] = s[j];
+			str2[1] = s[j + 1];
+			str2[2] = s[j + 2];
+			str2[3] = '\0';
+
+			if (!strcmp(str1, str2)) l[nl++] = j - i;
+
+		}
+	}
+
+	for (i = 0; i<nl; ++i)
+	for (j = i + 1; j<nl; ++j)
+		nods[nod(l[i], l[j])]++;
+
+
+	keylen = 0;
+	for (i = 2; i<500; ++i)
+	if (nods[keylen]<nods[i]) keylen = i;
+
+	//printf("%d\n", keylen);
+
+	free(s);
+
+
+	return keylen;
 }
